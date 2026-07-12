26e5b8842482f2bd5e25e4079ce48877c882076a
Gerard <lord.smeagol13@gmail.com>
2026-07-12 12:16:26 +0200

    fix: remove build artifacts from git tracking

--- TUI-OP-HUB/target/debug/deps/tui_op_hub-d3a4ec37ec42165d.d
+++
@@ -1,5 +0,0 @@
-/mnt/data/Fabian/Development/Projects/TUI-OP-HUB/TUI-OP-HUB/target/debug/deps/tui_op_hub-d3a4ec37ec42165d.d: src/main.rs
-
-/mnt/data/Fabian/Development/Projects/TUI-OP-HUB/TUI-OP-HUB/target/debug/deps/tui_op_hub-d3a4ec37ec42165d: src/main.rs
-
-src/main.rs:
--- TUI-OP-HUB/target/debug/tui-op-hub.d
+++
@@ -1,1 +0,0 @@
-/mnt/data/Fabian/Development/Projects/TUI-OP-HUB/TUI-OP-HUB/target/debug/tui-op-hub: /mnt/data/Fabian/Development/Projects/TUI-OP-HUB/TUI-OP-HUB/src/main.rs
